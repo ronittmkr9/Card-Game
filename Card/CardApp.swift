4c6eff60df714e02beca7c0805dcdbd38675212a
Ronit Tamrakar <ronittmkr17@gmail.com>
2024-08-17 14:24:26 +0545

    Initial Commit

---
+++ Card/CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardApp.swift
+//  Card
+//
+//  Created by Ashwin Tamrakar on 17/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
